10f3655a683c1139ebeacde2e7c2897f75e4f5d3
shenzhen <1624560576@qq.com>
2018-01-16 16:23:21 +0800

    用代码去连接指定的无线网络。

--- app/src/main/java/com/brick/robotctrl/SplashActivity.java
+++ app/src/main/java/com/brick/robotctrl/SplashActivity.java
@@ -11,10 +11,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.presentation.SamplePresentation;
 import com.rg2.activity.BaseActivity;
+import com.rg2.utils.WifiAdmin;
 
 /**
  * Created by shenzhen on 2017/1/7.
@@ -23,9 +25,11 @@ import com.rg2.activity.BaseActivity;
 public class SplashActivity extends BaseActivity {
 
     private static final String TAG ="SplashActivity" ;
+    public  String OPPOR9sk = '"'+"OPPO R9sk"+'"';
     private SamplePresentation mPresentation;
     private VideoView vv;
     private  Uri mUri;
+    public WifiAdmin wifiAdmin;
 
 
     @Override
@@ -98,6 +102,7 @@ public class SplashActivity extends BaseActivity {
     protected void onResume() {
         // Log.d(TAG, "onResume: ");
         super.onResume();
+        autoConnectWifi();
         // Register a callback for all events related to live video devices
        // mMediaRouter.addCallback(MediaRouter.ROUTE_TYPE_LIVE_VIDEO, mMediaRouterCallback);
         // Update the displays based on the currently active routes
@@ -154,6 +159,22 @@ public class SplashActivity extends BaseActivity {
                 // Log.d(TAG, "updatePresentation: failed");
                 mPresentation = null;
             }
+        }
+    }
+    //代码连接无线网
+    public void autoConnectWifi(){
+           wifiAdmin = new WifiAdmin(SplashActivity.this);
+        if(!OPPOR9sk.equals(wifiAdmin.getSSID())) {//如果已经里连接默认网络不要重复连接；
+            wifiAdmin.openWifi();
+            try {
+                //防止wifi 是关闭的，打开后要等待一段时间才能创建连接，否者会出现空指针
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            wifiAdmin.addNetwork(wifiAdmin.CreateWifiInfo("OPPO R9sk", "12345678", 3));
+            Toast.makeText(SplashActivity.this, "准备连接网络", Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "默认网络:");
         }
     }
 
